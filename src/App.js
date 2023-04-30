2ecfd184a7a27ec52f1b036f910de1e4f664fb98
CourtneyGoch <cgochenour.dev@gmail.com>
2023-04-30 19:55:01 -0400

    reworking layout - new app.js and index.js base

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+import { DataProvider } from "./context/DataContext";
+
+function App() {
+
+
+
+  return (
+    <>
+      <DataProvider>
+          <NavBar />
+          <Home />
+          <About />
+          <Portfolio />
+          <Experience />
+          <Contact />
+          <SocialLinks />
+      </DataProvider>
+    </>
+  );
+}
+
+export default App;
